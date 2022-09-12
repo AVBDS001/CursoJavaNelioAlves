68035d92d139548c5a491b36a8cb1dd821fb3cd0
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-09-12 09:52:35 -0300

    Create ContagemVotos.java

---
+++ Eleitor/src/entities/ContagemVotos.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entities;
+
+/**
+ *
+ * @author aluno
+ */
+public class ContagemVotos {
+    protected double  votosBrancos;
+    protected double votosNulos;
+    protected double votosValidos;
+    protected double totalVotos;
+    
+    public ContagemVotos() {
+    }
+
+    public ContagemVotos(double votosBrancos, double votosNulos, double votosValidos, double totalVotos) {
+        this.votosBrancos = votosBrancos;
+        this.votosNulos = votosNulos;
+        this.votosValidos = votosValidos;
+        this.totalVotos = totalVotos;
+    }
+
+    public double getVotosBrancos() {
+        return votosBrancos;
+    }
+
+    public double getVotosNulos() {
+        return votosNulos;
+    }
+  
+    public double getVotosValidos() {
+        return votosValidos;
+    }
+    
+    public double getTotalVotos() {
+        return totalVotos;
+    }
+    
+    public double contagem(double voto) {
+        double percentage;
+        return percentage = voto * 100 / totalVotos;
+    }
+}
