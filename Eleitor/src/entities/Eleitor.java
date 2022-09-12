db2be878829c2a79be0602fe8ee71cd66792e06c
André Vinicius Bezerra da Silva <105171947+AVBDS001@users.noreply.github.com>
2022-09-12 09:53:14 -0300

    Update Eleitor.java

--- Eleitor/src/entities/Eleitor.java
+++ Eleitor/src/entities/Eleitor.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entities;
 
-/**
- *
- * @author aluno
- */
+
 public class Eleitor {
     private String name;
     private String voteLocation;
